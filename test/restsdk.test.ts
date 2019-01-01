9f9ce0275752a3115d4c6abcd5339bff302cf027
Edoardo Gallo <edo91.gallo@gmail.com>
2019-01-01 23:21:31 +0100

    Run tests

--- test/restsdk.test.ts
+++ test/restsdk.test.ts
@@ -21,4 +21,16 @@ describe('RestClient', function () {
     response.dial({ callerId: from }, '+11111111111')
     expect(response.toString()).toEqual(`<?xml version="1.0" encoding="UTF-8"?><Response><Dial callerId="${from}">+11111111111</Dial></Response>`)
   })
+
+  it('can receive a fax', function () {
+    const response = new RestClient.LaML.FaxResponse()
+    response.receive({ action: '/receive/fax' })
+    expect(response.toString()).toEqual('<?xml version="1.0" encoding="UTF-8"?><Response><Receive action=\"/receive/fax\"/></Response>')
+  })
+
+  it('can reject a fax', function () {
+    const response = new RestClient.LaML.FaxResponse()
+    response.reject()
+    expect(response.toString()).toEqual('<?xml version="1.0" encoding="UTF-8"?><Response><Reject/></Response>')
+  })
 })

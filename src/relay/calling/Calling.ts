f46caaf13aa2d644a451e2e105b7321c0e9147b6
Edoardo Gallo <edo91.gallo@gmail.com>
2019-01-29 12:03:54 +0100

    relay onInboundCall method

--- src/relay/calling/Calling.ts
+++ src/relay/calling/Calling.ts
@@ -1,12 +1,28 @@
 // import logger from '../util/logger'
 import Relay from '../Relay'
 import { PhoneCall } from './Call';
+import logger from '../../util/logger';
+import { Execute } from '../../messages/Blade';
 
 export default class Calling extends Relay {
   service = 'calling'
 
   async makeCall(from: string, to: string) {
-    await this.setup()
+    // await this.setup()
+
     return new PhoneCall(this, { from, to })
+  }
+
+  async onInbound(context: string, handler: Function) {
+    // await this.setup()
+
+    const msg = new Execute({
+      protocol: this.protocol,
+      method: 'call.receive',
+      params: { context }
+    })
+
+    const result = await this.session.execute(msg).catch(error => error)
+    logger.debug('Register onInbound call:', result)
   }
 }

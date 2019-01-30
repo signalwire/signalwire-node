7a66152c5d439a76a9cdedc1cf96f8a6e99bfc5a
Edoardo Gallo <edo91.gallo@gmail.com>
2019-01-31 00:51:03 +0100

    wip: trigger inbound call notification

--- src/relay/calling/Calling.ts
+++ src/relay/calling/Calling.ts
@@ -3,10 +3,11 @@ import Relay from '../Relay'
 import { PhoneCall } from './Call';
 import logger from '../../util/logger';
 import { Execute } from '../../messages/Blade';
-import { trigger } from '../../services/Handler';
+import { trigger, register } from '../../services/Handler';
 
 export default class Calling extends Relay {
   service = 'calling'
+  private _inboundUniqueId = 'inbound'
 
   notificationHandler(notification: any) {
     logger.warn(`Relay ${this.service} notification on proto ${this._protocol}`, notification)
@@ -16,8 +17,22 @@ export default class Calling extends Relay {
         const { call_id, call_state, node_id } = params
         trigger(call_id, null, call_state, false)
         break
-      case 'calling.call.receive':
+      case 'calling.call.receive': {
+        const { type } = params
+        let call = null
+        if (type === 'phone') {
+          call = new PhoneCall(this, params)
+        } else if (type === 'sip') {
+          // call = new SipCall(this, params)
+        } else if (type === 'webrtc') {
+          // call = new SipCall(this, params)
+        } else {
+          logger.error('Received an unknown call type:', type, notification)
+          return
+        }
+        trigger(this._protocol, call, this._inboundUniqueId)
         break
+      }
       case 'calling.call.connect':
         break
     }
@@ -40,5 +55,6 @@ export default class Calling extends Relay {
 
     const result = await this.session.execute(msg).catch(error => error)
     logger.debug('Register onInbound call:', result)
+    register(this._protocol, handler, this._inboundUniqueId)
   }
 }

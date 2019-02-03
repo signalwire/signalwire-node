9ab75582304e8d319de2bfe263ecb8437cf15e7e
Edoardo Gallo <edo91.gallo@gmail.com>
2019-02-03 23:06:24 +0100

    wip: export client from a module and create a dedicated laml file

---
+++ test/client.ts
@@ -0,0 +1,9 @@
+require('dotenv').config()
+
+process.env.SIGNALWIRE_API_HOSTNAME = 'example.signalwire.com'
+process.env.SIGNALWIRE_API_PROJECT = 'XXXXXXXX-XXXX-XXXX-XXXX-XXXXXXXXXXXX'
+process.env.SIGNALWIRE_API_TOKEN = 'PTXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX'
+
+const RestClient = require('..').RestClient
+
+export default new RestClient(process.env.SIGNALWIRE_API_PROJECT, process.env.SIGNALWIRE_API_TOKEN)
--- test/laml.test.ts
+++ test/laml.test.ts
@@ -1,25 +1,12 @@
-require('dotenv').config()
-
 const RestClient = require('..').RestClient
 
 describe('RestClient', function () {
-  it('should call a number and return a call SID', function () {
-    const client = new RestClient(process.env.SIGNALWIRE_API_PROJECT, process.env.SIGNALWIRE_API_TOKEN);
-
-    client.calls.create({
-      url: 'http://demo.signalwire.com/laml/voice.xml',
-      to: '+11111111111',
-      from: process.env.SIGNALWIRE_TEST_NUMBER
-    }).then(call => {
-      expect(call).toHaveProperty('sid')
-    });
-  })
+  const FROM = '+11111111119'
 
   it('should generate LaML', function () {
     const response = new RestClient.LaML.VoiceResponse()
-    const from = process.env.SIGNALWIRE_TEST_NUMBER
-    response.dial({ callerId: from }, '+11111111111')
-    expect(response.toString()).toEqual(`<?xml version="1.0" encoding="UTF-8"?><Response><Dial callerId="${from}">+11111111111</Dial></Response>`)
+    response.dial({ callerId: FROM }, '+11111111111')
+    expect(response.toString()).toEqual(`<?xml version="1.0" encoding="UTF-8"?><Response><Dial callerId="${FROM}">+11111111111</Dial></Response>`)
   })
 
   it('can receive a fax', function () {

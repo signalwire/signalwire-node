3d668991e44619adc8705458ff829cb3aa99612c
Edoardo Gallo <edo91.gallo@gmail.com>
2019-02-18 11:45:17 +0100

    add unit tests to check queue logic

--- packages/common/tests/behaveLike/BladeMessages.ts
+++ packages/common/tests/behaveLike/BladeMessages.ts
@@ -1,5 +1,6 @@
 import { trigger } from '../../src/services/Handler'
 import { SwEvent } from '../../src/util/constants'
+import { Execute } from '../../src/messages/Blade'
 
 /**
  * Module to test "blade.xxx" messages in both browser/node following DRY principle.
@@ -11,14 +12,29 @@ const BladeDisconnect = (klass: any) => {
     beforeAll(async done => {
       instance = new klass({ host: 'example.signalwire.com', project: 'project', token: 'token' })
       await instance.connect()
+
+      const msg = JSON.parse('{"id":"378d7dea-e581-4305-a7e7-d29173797f32","jsonrpc":"2.0","method":"blade.disconnect"}')
+      trigger(SwEvent.SocketMessage, msg, instance.uuid)
       done()
     })
 
     it('should put the session in a idle state', () => {
-      const msg = JSON.parse('{"id":"378d7dea-e581-4305-a7e7-d29173797f32","jsonrpc":"2.0","method":"blade.disconnect"}')
-      trigger(SwEvent.SocketMessage, msg, instance.uuid)
-      // @ts-ignore
       expect(instance._idle).toEqual(true)
+    })
+
+    it('should queue subsequent requests', () => {
+      const be = new Execute({ protocol: 'test', method: 'fake', params: {} })
+      instance.execute(be).catch(error => error)
+      expect(instance._executeQueue).toHaveLength(1)
+      expect(instance._executeQueue[0]).toHaveProperty('msg')
+      expect(instance._executeQueue[0]).toHaveProperty('resolve')
+    })
+
+    it('should queue subsequent raw requests', () => {
+      instance.executeRaw('#TEST')
+      expect(instance._executeQueue).toHaveLength(2)
+      expect(instance._executeQueue[1]).toHaveProperty('msg')
+      expect(instance._executeQueue[1]).not.toHaveProperty('resolve')
     })
   })
 }

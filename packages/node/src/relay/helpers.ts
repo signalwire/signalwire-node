2cb76f75ab61c0fc0ab155821fec12c19745cab7
Edoardo Gallo <edo91.gallo@gmail.com>
2019-02-26 15:42:22 +0100

    Call type as param (#27)
    * removing method to cleanup number and input parameters
    
    * accept parameter 'type' on relay makeCall method
    
    * Passing jest

--- packages/node/src/relay/helpers.ts
+++ packages/node/src/relay/helpers.ts
@@ -1,56 +1,20 @@
-import { CallType } from '../../../common/src/util/constants/relay'
-import { cleanNumber } from '../../../common/src/util/helpers'
-import { ICallDevice, IMakeCallParams } from '../../../common/src/util/interfaces'
+import { ICallDevice } from '../../../common/src/util/interfaces'
 
 interface DeepArray<T> extends Array<T | DeepArray<T>> { }
-
-export const detectCallType = (_to: string = ''): CallType => {
-  let to: string = ''
-  try { to = _to.trim() } catch {}
-  if (!to) {
-    return null
-  }
-  if (/^sip:/.test(to)) {
-    return CallType.Sip
-  }
-  if (cleanNumber(to)) {
-    return CallType.Phone
-  }
-  return CallType.WebRTC
-}
 
 interface DeviceAccumulator {
   devices: DeepArray<ICallDevice>,
   nested: boolean
 }
 
-export const cleanCallingParams = (params: IMakeCallParams): { type: CallType, from_number: string, to_number: string, timeout: number } => {
-  const { from = '', to = '', timeout = 30 } = params
-  const type = detectCallType(to)
-  switch (type) {
-    case CallType.Phone:
-      return { type, from_number: cleanNumber(from), to_number: cleanNumber(to), timeout: Number(timeout) }
-    case CallType.Sip:
-    case CallType.WebRTC:
-      return { type, from_number: from.trim(), to_number: to.trim(), timeout: Number(timeout) }
-  }
-  return { type, from_number: from, to_number: to, timeout: Number(timeout) }
-}
-
 export const reduceConnectParams = (peers: any[], callDevice: ICallDevice): DeepArray<ICallDevice> => {
   const { params: { from_number: defaultFromNumber, timeout: defaultTimeout } } = callDevice
-  const _reducer = (accumulator: DeviceAccumulator, peer: any, currentIndex: number, sourceArray: any[]) => {
+  const _reducer = (accumulator: DeviceAccumulator, peer: any) => {
     let tmp: ICallDevice = null
     if (peer instanceof Array) {
       tmp = peer.reduce(_reducer, { devices: [], nested: true }).devices
-    } else if (typeof peer === 'string') {
-      const { type, from_number, to_number, timeout } = cleanCallingParams({ from: defaultFromNumber, to: peer, timeout: defaultTimeout })
-      if (type) {
-        tmp = { type, params: { to_number, from_number, timeout } }
-      }
     } else if (typeof peer === 'object') {
-      const { to, from = defaultFromNumber, timeout: _timeout = defaultTimeout } = peer
-      const { type, from_number, to_number, timeout } = cleanCallingParams({ from, to, timeout: _timeout })
+      const { type, from: from_number = defaultFromNumber, to: to_number, timeout = defaultTimeout } = peer
       if (type) {
         tmp = { type, params: { to_number, from_number, timeout } }
       }

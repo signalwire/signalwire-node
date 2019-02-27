d4f9ba4360a7115e475fd35723eb2b922fd8d5d1
Edoardo Gallo <edo91.gallo@gmail.com>
2019-02-27 14:30:18 +0100

    Add configure method for SIP and WebRTC and catch error on Setup

--- packages/common/src/relay/Relay.ts
+++ packages/common/src/relay/Relay.ts
@@ -1,7 +1,9 @@
 import BaseSession from '../BaseSession'
+import { Execute } from '../messages/Blade'
 import { Setup } from '../services/Setup'
 import { registerOnce, deRegisterAll } from '../services/Handler'
 import { SwEvent } from '../util/constants'
+import logger from '../util/logger'
 
 abstract class Relay {
   protected abstract service: string
@@ -17,9 +19,30 @@ abstract class Relay {
   }
 
   protected async setup() {
-    if (!this._protocol) {
-      this._protocol = await Setup(this.session, this.service, this.notificationHandler.bind(this))
+    if (this._protocol) {
+      return
     }
+    this._protocol = await Setup(this.session, this.service, this.notificationHandler.bind(this))
+      .catch(error => {
+        logger.error(`Error during setup ${this.service}.`, error)
+        return null
+      })
+  }
+
+  protected async configure() {
+    if (!this._protocol) {
+      throw new Error('No protocol to configure.')
+    }
+    // TODO: add 'resource' - 'domain' to ISignalWireOptions interface
+    // @ts-ignore
+    const { resource = 'swire', domain = 'dev.swire.io' } = this.session.options
+    const msg = new Execute({ protocol: this._protocol, method: 'configure', params: { resource, domain } })
+    const response = await this.session.execute(msg)
+      .catch(error => {
+        logger.error(`Error during configure ${this.service}.`, error)
+        return null
+      })
+    console.log('Configure', response)
   }
 
   protected _disconnect() {

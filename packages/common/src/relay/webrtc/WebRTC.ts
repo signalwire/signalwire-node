affc2e3ccebea9e22b9ae8410f18d81a89262f44
Edoardo Gallo <edo91.gallo@gmail.com>
2019-02-27 14:30:42 +0100

    wip: initial webrtc tests

---
+++ packages/common/src/relay/webrtc/WebRTC.ts
@@ -0,0 +1,53 @@
+// import { Execute } from '../../messages/Blade'
+// import { isFunction } from '../../util/helpers'
+// import { register } from '../../services/Handler'
+
+import logger from '../../util/logger'
+import Relay from '../Relay'
+import { DialogOptions } from '../../util/interfaces'
+import Dialog from '../../../../web/src/rtc/Dialog'
+import BrowserSession from '../../../../web/src/BrowserSession'
+
+import { Execute } from '../../messages/Blade'
+import { Login } from '../../messages/Verto'
+
+export default class WebRTC extends Relay {
+  service = 'webrtc'
+  // private _calls: Call[] = []
+
+  constructor(public session: BrowserSession) {
+    super(session)
+  }
+
+  notificationHandler(notification: any) {
+    const { event_type, params } = notification
+    console.log('WebRTC notification', event_type, params)
+    // switch (event_type) {
+    // }
+  }
+
+  async makeCall(params: DialogOptions) {
+    await this.setup()
+    await this.configure()
+
+    const msg = new Execute({
+      protocol: this._protocol, method: 'message', params: {
+        message: new Login('1008', '1234', null, {}).request
+      }
+    })
+    const response = await this.session.execute(msg)
+      .catch(error => {
+        logger.error('SignalWire login error', error)
+      })
+
+    // logger.info('webrtc makeCall', params)
+
+    // const { destinationNumber = null } = params
+    // if (!destinationNumber) {
+    //   throw new Error('SignalWire.newCall() error: destinationNumber is required.')
+    // }
+    // const dialog = new Dialog(this.session, params)
+    // dialog.invite()
+    // return dialog
+  }
+}
--- packages/web/src/SignalWire.ts
+++ packages/web/src/SignalWire.ts
@@ -1,7 +1,39 @@
 import BrowserSession from './BrowserSession'
+import WebRTC from '../../common/src/relay/webrtc/WebRTC'
+import { DialogOptions } from '../../common/src/util/interfaces'
+import logger from '../../common/src/util/logger'
+import { Execute } from '../../common/src/messages/Blade'
+import BaseRequest from '../../common/src/messages/verto/BaseRequest'
+import { Login } from '../../common/src/messages/Verto'
 
 export default class SignalWire extends BrowserSession {
+  private _webrtcInstance: WebRTC = null
+
   protected async _onDisconnect() {
     // TODO: sent unsubscribe for all subscriptions?
+  }
+
+  execute(message: any) {
+    let msg: any = message
+    if (message instanceof BaseRequest) {
+      msg = new Execute({ protocol: this._webrtcInstance.protocol, method: 'message', params: { message: message.request } })
+    }
+    return super.execute(msg)
+  }
+
+  _test() {
+    if (this._webrtcInstance === null) {
+      this._webrtcInstance = new WebRTC(this)
+    }
+  }
+
+  async newCall(options: DialogOptions) {
+    this._test()
+
+    const dialog = await this._webrtcInstance.makeCall(options)
+      .catch(error => {
+        logger.error('SignalWire newCall error', error)
+      })
+    return dialog
   }
 }

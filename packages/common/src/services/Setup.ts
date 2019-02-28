b2a890557fd71e45c2df386968fe25736a4aa1e0
Edoardo Gallo <edo91.gallo@gmail.com>
2019-02-28 11:08:35 +0100

    Cleanup Setup and Configure leaving handling error to the higher level functions

--- packages/common/src/services/Setup.ts
+++ packages/common/src/services/Setup.ts
@@ -8,15 +8,7 @@ const SETUP_CHANNEL = 'notifications'
 export const Setup = async (session: BaseSession, service: string, handler?: Function): Promise<string> => {
   const be = new Execute({ protocol: SETUP_PROTOCOL, method: SETUP_METHOD, params: { service } })
   const { result: { protocol = null } = {} } = await session.execute(be)
-    .catch(error => error)
-  if (protocol === null) {
-    throw new Error(`Setup '${service}' error: invalid execute.`)
-  }
-  const { subscribe_channels = [], failed_channels = [] } = await session.subscribe({ protocol, channels: [SETUP_CHANNEL], handler })
-    .catch(error => error)
-  if (failed_channels.length || subscribe_channels.indexOf(SETUP_CHANNEL) < 0) {
-    throw new Error(`Setup '${service}' error: invalid subscription to channel.`)
-  }
+  await session.subscribe({ protocol, channels: [SETUP_CHANNEL], handler })
 
   return protocol
 }

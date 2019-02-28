9fa7214a4f8bc83a0d42d4c07dece29f4a6dc707
Edoardo Gallo <edo91.gallo@gmail.com>
2019-02-28 10:16:08 +0100

    Send 'configure' message based from an instance variable

--- packages/common/src/relay/Relay.ts
+++ packages/common/src/relay/Relay.ts
@@ -7,6 +7,7 @@ abstract class Relay {
   protected abstract service: string
   protected _protocol: string // TODO: public removing GETTER
   protected abstract notificationHandler(notification: any): void
+  protected _configure: boolean = false
 
   constructor(public session: BaseSession) {
   }
@@ -24,22 +25,20 @@ abstract class Relay {
         logger.error(`Error during setup ${this.service}.`, error)
         return null
       })
+    if (this._protocol && this._configure) {
+      await this.configure()
+    }
   }
 
   protected async configure() {
-    if (!this._protocol) {
-      throw new Error('No protocol to configure.')
-    }
     // TODO: add 'resource' - 'domain' to ISignalWireOptions interface
     // @ts-ignore
     const { resource = 'swire', domain = 'dev.swire.io' } = this.session.options
     const msg = new Execute({ protocol: this._protocol, method: 'configure', params: { resource, domain } })
-    const response = await this.session.execute(msg)
+    await this.session.execute(msg)
       .catch(error => {
-        logger.error(`Error during configure ${this.service}.`, error)
-        return null
+        throw error.result
       })
-    console.log('Configure', response)
   }
 }
 

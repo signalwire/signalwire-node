c7bf02d4367ff78de8526fdabde1f326a5d70882
Edoardo Gallo <edo91.gallo@gmail.com>
2019-03-11 16:11:07 +0100

    Replace console.log with loglevel pck

--- packages/common/src/util/logger.ts
+++ packages/common/src/util/logger.ts
@@ -1,40 +1,20 @@
-// import winston from 'winston'
-
-// const logger = new winston.Logger({
-//   transports: [
-//     new winston.transports.Console({
-//       name: 'debug-console',
-//       level: process.env.NODE_ENV === 'production' ? 'error' : 'debug',
-//       prettyPrint: true,
-//       handleExceptions: true,
-//       // json: false,
-//       colorize: true
-//     })
-//   ],
-//   exitOnError: false
-// });
-
-// export default logger;
-// declare var ENV: string
-
+import * as log from 'loglevel'
 const datetime = () => new Date().toISOString().replace('T', ' ').replace('Z', '')
-const _do = (type: string, args: any) => {
-  console[type](datetime(), '::', ...args)
-}
+const logger = log.getLogger('signalwire')
+// logger.setLevel(log.levels.WARN, true)
+// logger.enableAll()
 
-const logger = {
-  info: (...args) => {
-    process.env.NODE_ENV === 'development' ? _do('info', args) : null
-  },
-  debug: (...args) => {
-    process.env.NODE_ENV === 'development' ? _do('debug', args) : null
-  },
-  warn: (...args) => {
-    _do('warn', args)
-  },
-  error: (...args) => {
-    _do('error', args)
+const originalFactory = logger.methodFactory
+logger.methodFactory = (methodName, logLevel, loggerName) => {
+  const rawMethod = originalFactory(methodName, logLevel, loggerName)
+  return function () {
+    const messages = [datetime(), '-']
+    for (let i = 0; i < arguments.length; i++) {
+      messages.push(arguments[i])
+    }
+    rawMethod.apply(undefined, messages)
   }
 }
+logger.setLevel(logger.getLevel())
 
 export default logger

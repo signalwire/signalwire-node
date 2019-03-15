0049e961c1e734b7275d379acaf79a272ef3ba7e
Edoardo Gallo <edo91.gallo@gmail.com>
2019-03-15 16:18:06 +0100

    Initial script to setup dev environment

---
+++ .scripts/setup.js
@@ -0,0 +1,14 @@
+const common = require('./common')
+const Listr = require('listr')
+
+
+async function main() {
+  const tasks = []
+  const input = process.argv[2]
+  common.packages
+    .filter(package => package === 'common' || (!input || input === package))
+    .forEach(package => common.setupPackage(tasks, package))
+  await new Listr(tasks, { showSubtasks: true }).run()
+}
+
+main()

c93fe565880b09934d13f5a1e3b24a4593e96040
Edoardo Gallo <edo91.gallo@gmail.com>
2019-03-25 10:17:58 +0100

    rm unused fn

--- packages/common/src/util/helpers.ts
+++ packages/common/src/util/helpers.ts
@@ -1,19 +1,5 @@
 import logger from './logger'
-import { ISignalWireOptions } from './interfaces'
 import { STORAGE_PREFIX } from './constants'
-
-export const validateOptions = (options: ISignalWireOptions, className: string): boolean => {
-  let check: boolean = false
-  const { host, project, token, login, passwd, password } = options
-  if (className === 'SignalWire') {
-    check = Boolean(project && token)
-  } else if (className === 'Verto') {
-    check = Boolean(login && (passwd || password))
-  } else {
-    return false
-  }
-  return Boolean(host) && check
-}
 
 export const objEmpty = (obj: Object) => Object.keys(obj).length === 0
 

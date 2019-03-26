662ed4bdf7e8111314f4938ee15142ac46ba039b
Edoardo Gallo <edo91.gallo@gmail.com>
2019-03-26 21:05:59 +0100

    wip: postinstall scripts

---
+++ packages/react-native/.scripts/postinstall.js
@@ -0,0 +1,59 @@
+const execa = require('execa')
+// const path = require('path')
+const Listr = require('listr')
+
+function cwd() {
+  return process.env.INIT_CWD
+}
+
+async function main() {
+  const mainTasks = [
+    {
+      title: 'Checking react-native..',
+      task: async () => await execa('react-native', ['--version'], { cwd: cwd() }).catch(error => {
+        throw new Error('\n\n\treact-native CLI not available.\n\n')
+      })
+    },
+    {
+      title: 'Installing WebRTC engine..',
+      task: async () => await execa('npm', ['install', 'react-native-webrtc'], { cwd: cwd() })
+    },
+    {
+      title: 'Linking native libraries...\n',
+      task: () => new Listr([ androidTasks(), iosTasks() ])
+    }
+  ]
+
+  await new Listr(mainTasks, { showSubtasks: true }).run().catch(e => console.error(e.message))
+}
+
+function androidTasks() {
+  const tasks = [
+    {
+      title: `Link WebRTC engine to Android..`,
+      task: async () => await execa('react-native', ['link', 'react-native-webrtc', 'android'], { cwd: cwd() })
+    }
+  ]
+
+  return {
+    title: 'Checking Android...',
+    task: () => new Listr(tasks)
+  }
+}
+
+function iosTasks() {
+  // TODO: check Podfile for iOS
+  const tasks = [
+    {
+      title: `Link WebRTC engine to iOS..`,
+      task: async () => await execa('react-native', ['link', 'react-native-webrtc', 'ios'], { cwd: cwd() })
+    }
+  ]
+
+  return {
+    title: 'Checking iOS...',
+    task: () => new Listr(tasks)
+  }
+}
+
+main()

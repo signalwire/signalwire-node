4e6d649946255948d32acafe0ea327c1157e805a
Edoardo Gallo <edo91.gallo@gmail.com>
2019-04-23 17:41:19 +0200

    improve jests

--- packages/node/tests/relay/Call.test.ts
+++ packages/node/tests/relay/Call.test.ts
@@ -8,12 +8,6 @@ const Connection = require('../../../common/src/services/Connection')
 jest.mock('../../../common/src/services/Connection')
 
 describe('Call', () => {
-  // const mockSetupResponses = () => {
-  //   Connection.mockResponse
-  //     .mockImplementationOnce(() => JSON.parse('{"id":"c04d725a-c8bc-4b9e-bf1e-9c05150797cc","jsonrpc":"2.0","result":{"requester_nodeid":"05b1114c-XXXX-YYYY-ZZZZ-feaa30afad6c","responder_nodeid":"9811eb32-XXXX-YYYY-ZZZZ-ab56fa3b83c9","result":{"protocol":"signalwire_service_random_uuid"}}}'))
-  //     .mockImplementationOnce(() => JSON.parse('{"id":"24f9b545-8bed-49e1-8214-5dbadb545f7d","jsonrpc":"2.0","result":{"command":"add","failed_channels":[],"protocol":"signalwire_service_random_uuid","subscribe_channels":["notifications"]}}'))
-  // }
-
   let session: RelayClient = null
 
   beforeAll(done => {
@@ -29,8 +23,6 @@ describe('Call', () => {
   let call: Call = null
   beforeEach(() => {
     Connection.mockSend.mockClear()
-    // mockSetupResponses()
-
     Connection.mockResponse
       .mockReturnValueOnce(JSON.parse('{"id":"c04d725a-c8bc-4b9e-bf1e-9c05150797cc","jsonrpc":"2.0","result":{"requester_nodeid":"05b1114c-XXXX-YYYY-ZZZZ-feaa30afad6c","responder_nodeid":"9811eb32-XXXX-YYYY-ZZZZ-ab56fa3b83c9","result":{"protocol":"signalwire_service_random_uuid"}}}'))
       .mockReturnValueOnce(JSON.parse('{"id":"24f9b545-8bed-49e1-8214-5dbadb545f7d","jsonrpc":"2.0","result":{"command":"add","failed_channels":[],"protocol":"signalwire_service_random_uuid","subscribe_channels":["notifications"]}}'))
--- packages/node/tests/relay/calling.test.ts
+++ packages/node/tests/relay/calling.test.ts
@@ -10,10 +10,9 @@ describe('Calling', () => {
   let session: RelayClient = null
 
   const _common = () => {
-    beforeAll(async done => {
+    beforeAll(done => {
       session = new RelayClient({ host: 'example.signalwire.com', project: 'project', token: 'token' })
-      await session.connect()
-      done()
+      session.connect().then(done)
     })
 
     afterAll(() => {
@@ -30,15 +29,11 @@ describe('Calling', () => {
     })
   }
 
-  const mockSetupResponses = () => {
-    Connection.mockResponse
-      .mockImplementationOnce(() => JSON.parse('{"id":"c04d725a-c8bc-4b9e-bf1e-9c05150797cc","jsonrpc":"2.0","result":{"requester_nodeid":"05b1114c-XXXX-YYYY-ZZZZ-feaa30afad6c","responder_nodeid":"9811eb32-XXXX-YYYY-ZZZZ-ab56fa3b83c9","result":{"protocol":"signalwire_service_random_uuid"}}}'))
-      .mockImplementationOnce(() => JSON.parse('{"id":"24f9b545-8bed-49e1-8214-5dbadb545f7d","jsonrpc":"2.0","result":{"command":"add","failed_channels":[],"protocol":"signalwire_service_random_uuid","subscribe_channels":["notifications"]}}'))
-  }
-
   beforeEach(() => {
-    mockSetupResponses()
     Connection.mockSend.mockClear()
+    Connection.mockResponse
+      .mockReturnValueOnce(JSON.parse('{"id":"c04d725a-c8bc-4b9e-bf1e-9c05150797cc","jsonrpc":"2.0","result":{"requester_nodeid":"05b1114c-XXXX-YYYY-ZZZZ-feaa30afad6c","responder_nodeid":"9811eb32-XXXX-YYYY-ZZZZ-ab56fa3b83c9","result":{"protocol":"signalwire_service_random_uuid"}}}'))
+      .mockReturnValueOnce(JSON.parse('{"id":"24f9b545-8bed-49e1-8214-5dbadb545f7d","jsonrpc":"2.0","result":{"command":"add","failed_channels":[],"protocol":"signalwire_service_random_uuid","subscribe_channels":["notifications"]}}'))
   })
 
   describe('.newCall()', () => {
@@ -68,13 +63,12 @@ describe('Calling', () => {
 
     beforeEach(async done => {
       fnMock.mockClear()
-      await session.calling.onInbound(context, fnMock)
-      done()
+      session.calling.onInbound(context, fnMock).then(done)
     })
 
     it('should reuse the protocol setupped previously', () => {
       expect(Connection.mockSend).toHaveBeenCalledTimes(1)
-      const { method } =  Connection.mockSend.mock.calls[0][0].request.params
+      const { method } = Connection.mockSend.mock.calls[0][0].request.params
       expect(method).toEqual('call.receive')
       expect(session.calling.protocol).toEqual('signalwire_service_random_uuid')
     })
@@ -85,7 +79,7 @@ describe('Calling', () => {
       expect(isQueued(protocol, uniqueId)).toEqual(true)
     })
 
-    it('should handle the notification', () => {
+    it('should handle the calling.call.receive notification', () => {
       const msg = JSON.parse('{"id":"a16dae67-212b-4391-bb78-a0b9e45310b9","jsonrpc":"2.0","method":"blade.broadcast","params":{"broadcaster_nodeid":"9811eb32-1234-1234-1234-ab56fa3b83c9","channel":"notifications","event":"relay","params":{"event_channel":"signalwire_service_random_uuid","event_type":"calling.call.receive","params":{"call_id":"849982ab-1234-5678-1234-311534fa20d6","call_state":"created","context":"context","device":{"params":{"from_number":"+12222222222","to_number":"+12222222223"},"type":"phone"},"node_id":"9811eb32-1234-5678-XXXX-ab56fa3b83c9"},"project_id":"project","space_id":"space","timestamp":1149889452.302629},"protocol":"signalwire_service_random_uuid"}}')
       trigger(SwEvent.SocketMessage, msg, session.uuid)
       expect(fnMock).toHaveBeenCalled()

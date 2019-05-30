1c5da6559c60804fdda71796b8af1d86fed31ce8
Edoardo Gallo <edo91.gallo@gmail.com>
2019-05-30 12:08:48 +0200

    Prevent error using localStorage in Nodejs

--- packages/common/src/util/storage/index.ts
+++ packages/common/src/util/storage/index.ts
@@ -1,18 +1,31 @@
 import { mutateStorageKey, safeParseJson } from '../helpers'
 
+const _inNode = () => typeof window === 'undefined' && typeof process !== 'undefined'
+
 const getItem = async (key: string): Promise<any> => {
+  if (_inNode()) {
+    return null
+  }
   const res = window.localStorage.getItem(mutateStorageKey(key))
   return safeParseJson(res)
 }
 
 const setItem = async (key: string, value: any): Promise<void> => {
+  if (_inNode()) {
+    return null
+  }
   if (typeof value === 'object') {
     value = JSON.stringify(value)
   }
   window.localStorage.setItem(mutateStorageKey(key), value)
 }
 
-const removeItem = async (key: string): Promise<void> => window.localStorage.removeItem(mutateStorageKey(key))
+const removeItem = async (key: string): Promise<void> => {
+  if (_inNode()) {
+    return null
+  }
+  return window.localStorage.removeItem(mutateStorageKey(key))
+}
 
 export {
   getItem,

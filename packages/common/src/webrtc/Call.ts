27186c914f23b990114393e9a4faa2836dee0346
Edoardo Gallo <edo91.gallo@gmail.com>
2019-06-06 19:44:07 +0200

    rename set device methods

--- packages/common/src/webrtc/Call.ts
+++ packages/common/src/webrtc/Call.ts
@@ -190,73 +190,81 @@ export default class Call {
     return `conference-member.${this.id}`
   }
 
-  get microphone() {
-    return {
-      mute: () => this.peer.audioState = 'off',
-      unmute: () => this.peer.audioState = 'on',
-      toggleMute: () => this.peer.audioState = 'toggle',
-      changeDevice: async (deviceId: string): Promise<void> => {
-        const { instance } = this.peer
-        const sender = instance.getSenders().find(({ track: { kind } }: RTCRtpSender) => kind === 'audio')
-        if (sender) {
-          const newStream = await getUserMedia({ audio: { deviceId: { exact: deviceId } } })
-          const audioTrack = newStream.getAudioTracks()[0]
-          sender.replaceTrack(audioTrack)
-          const { localStream } = this.options
-          localStream.getAudioTracks().forEach(track => {
-            localStream.removeTrack(track)
-            track.stop()
-          })
-          localStream.addTrack(audioTrack)
-          this.options.micId = deviceId
-        }
-      }
+  muteAudio() {
+    this.peer.audioState = 'off'
+  }
+
+  unmuteAudio() {
+    this.peer.audioState = 'on'
+  }
+
+  toggleAudioMute() {
+    this.peer.audioState = 'toggle'
+  }
+
+  async setAudioInDevice(deviceId: string): Promise<void> {
+    const { instance } = this.peer
+    const sender = instance.getSenders().find(({ track: { kind } }: RTCRtpSender) => kind === 'audio')
+    if (sender) {
+      const newStream = await getUserMedia({ audio: { deviceId: { exact: deviceId } } })
+      const audioTrack = newStream.getAudioTracks()[0]
+      sender.replaceTrack(audioTrack)
+      const { localStream } = this.options
+      localStream.getAudioTracks().forEach(track => {
+        localStream.removeTrack(track)
+        track.stop()
+      })
+      localStream.addTrack(audioTrack)
+      this.options.micId = deviceId
     }
   }
 
-  get webcam() {
-    return {
-      mute: () => this.peer.videoState = 'off',
-      unmute: () => this.peer.videoState = 'on',
-      toggleMute: () => this.peer.videoState = 'toggle',
-      changeDevice: async (deviceId: string): Promise<void> => {
-        const { instance } = this.peer
-        const sender = instance.getSenders().find(({ track: { kind } }: RTCRtpSender) => kind === 'video')
-        if (sender) {
-          const newStream = await getUserMedia({ video: { deviceId: { exact: deviceId } } })
-          const videoTrack = newStream.getVideoTracks()[0]
-          sender.replaceTrack(videoTrack)
-          const { localStream } = this.options
-          localStream.getVideoTracks().forEach(track => {
-            localStream.removeTrack(track)
-            track.stop()
-          })
-          localStream.addTrack(videoTrack)
-          this.options.camId = deviceId
-        }
-      }
+  muteVideo() {
+    this.peer.videoState = 'off'
+  }
+
+  unmuteVideo() {
+    this.peer.videoState = 'on'
+  }
+
+  toggleVideoMute() {
+    this.peer.videoState = 'toggle'
+  }
+
+  async setVideoDevice(deviceId: string): Promise<void> {
+    const { instance } = this.peer
+    const sender = instance.getSenders().find(({ track: { kind } }: RTCRtpSender) => kind === 'video')
+    if (sender) {
+      const newStream = await getUserMedia({ video: { deviceId: { exact: deviceId } } })
+      const videoTrack = newStream.getVideoTracks()[0]
+      sender.replaceTrack(videoTrack)
+      const { localStream } = this.options
+      localStream.getVideoTracks().forEach(track => {
+        localStream.removeTrack(track)
+        track.stop()
+      })
+      localStream.addTrack(videoTrack)
+      this.options.camId = deviceId
     }
   }
 
-  get speaker() {
-    return {
-      mute: () => {
-        const { remoteElement } = this.options
-        muteMediaElement(remoteElement)
-      },
-      unmute: () => {
-        const { remoteElement } = this.options
-        unmuteMediaElement(remoteElement)
-      },
-      changeDevice: async (deviceId: string): Promise<boolean> => {
-        this.options.speakerId = deviceId
-        const { remoteElement, speakerId } = this.options
-        if (remoteElement && speakerId) {
-          return setMediaElementSinkId(remoteElement, speakerId)
-        }
-        return false
-      }
+  deaf() {
+    const { remoteElement } = this.options
+    muteMediaElement(remoteElement)
+  }
+
+  undeaf() {
+    const { remoteElement } = this.options
+    unmuteMediaElement(remoteElement)
+  }
+
+  async setAudioOutDevice(deviceId: string): Promise<boolean> {
+    this.options.speakerId = deviceId
+    const { remoteElement, speakerId } = this.options
+    if (remoteElement && speakerId) {
+      return setMediaElementSinkId(remoteElement, speakerId)
     }
+    return false
   }
 
   setState(state: State) {

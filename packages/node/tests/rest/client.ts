2be57e57c66bfc4a92ee63158d5941a7b7b2589d
Edoardo Gallo <edo91.gallo@gmail.com>
2019-06-25 11:55:08 +0200

    fix jests

--- packages/node/tests/rest/client.ts
+++ packages/node/tests/rest/client.ts
@@ -4,4 +4,4 @@ const host = 'example.signalwire.com'
 const project = 'XXXXXXXX-XXXX-XXXX-XXXX-XXXXXXXXXXXX'
 const token = 'PTXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX'
 
-export default new RestClient(project, token, { signalwireSpaceUrl: host })
+export default RestClient(project, token, { signalwireSpaceUrl: host })

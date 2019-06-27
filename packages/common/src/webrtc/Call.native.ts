acdfd3f66ddf0688847db81f8126bcd302d35d5e
Edoardo Gallo <edo91.gallo@gmail.com>
2019-06-28 00:22:44 +0200

    start using incall-manager for RN Call

--- packages/common/src/webrtc/Call.native.ts
+++ packages/common/src/webrtc/Call.native.ts
@@ -1,33 +1,49 @@
 import BaseCall from './BaseCall'
 import { State } from '../util/constants/call'
+// @ts-ignore
+import InCallManager from 'react-native-incall-manager'
+import { streamIsValid } from '../util/webrtc'
 
 export default class Call extends BaseCall {
 
   switchCamera() {
-
+    const { localStream } = this.options
+    if (streamIsValid(localStream)) {
+      // @ts-ignore
+      localStream.getVideoTracks().forEach(t => t._switchCamera())
+    }
   }
 
   deaf() {
-
+    const { remoteStream } = this.options
+    if (streamIsValid(remoteStream)) {
+      remoteStream.getAudioTracks().forEach(t => t.enabled = false)
+    }
   }
 
   undeaf() {
-
+    const { remoteStream } = this.options
+    if (streamIsValid(remoteStream)) {
+      remoteStream.getAudioTracks().forEach(t => t.enabled = true)
+    }
   }
 
-  speakerphone() {
-
+  setSpeakerPhone(flag: boolean) {
+    InCallManager.setForceSpeakerphoneOn(flag)
   }
 
   setState(state: State) {
-    super.setState(state)
-
-    // TODO: react-native-incall-manager handling
     switch (state) {
       case State.Active:
+        InCallManager.start({
+          media: Boolean(this.options.video) ? 'video' : 'audio'
+        })
         break
       case State.Destroy:
+        InCallManager.stop()
         break
     }
+
+    super.setState(state)
   }
 }

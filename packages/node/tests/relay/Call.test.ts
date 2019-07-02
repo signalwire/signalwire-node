650df21967708ab78e90db061bdbec5d7a9457e3
Edoardo Gallo <edo91.gallo@gmail.com>
2019-07-02 19:50:22 +0200

    record and connect jests

--- packages/node/tests/relay/Call.test.ts
+++ packages/node/tests/relay/Call.test.ts
@@ -90,48 +90,49 @@ describe('Call', () => {
   })
 
   describe('when call is ready', () => {
-    const _stateNotificationAnswered = JSON.parse(`{"event_type":"calling.call.state","event_channel":"signalwire_service_random_uuid","project_id":"pid","space_id":"sid","params":{"call_state":"answered","direction":"inbound","device":{"type":"phone","params":{"from_number":"+1234","to_number":"15678"}},"call_id":"call-id","node_id":"node-id"}}`)
-    const _stateNotificationEnded = JSON.parse(`{"event_type":"calling.call.state","event_channel":"signalwire_service_random_uuid","project_id":"pid","space_id":"sid","params":{"call_state":"ended","end_reason":"busy","direction":"inbound","device":{"type":"phone","params":{"from_number":"+1234","to_number":"15678"}},"call_id":"call-id","node_id":"node-id"}}`)
+    const _stateNotificationAnswered = JSON.parse(`{"event_type":"calling.call.state","params":{"call_state":"answered","direction":"inbound","device":{"type":"phone","params":{"from_number":"+1234","to_number":"15678"}},"call_id":"call-id","node_id":"node-id"}}`)
+    const _stateNotificationEnded = JSON.parse(`{"event_type":"calling.call.state","params":{"call_state":"ended","end_reason":"busy","direction":"inbound","device":{"type":"phone","params":{"from_number":"+1234","to_number":"15678"}},"call_id":"call-id","node_id":"node-id"}}`)
+    const _recordNotification = JSON.parse(`{"event_type":"calling.call.record","params":{"state":"no_input","record":{"audio":{"format":"mp3","direction":"speak","stereo":false}},"url":"record.mp3","control_id":"mocked-uuid","size":4096,"duration":4,"call_id":"call-id","node_id":"node-id"}}`)
+    const _connectNotification = JSON.parse(`{"event_type":"calling.call.connect","params":{"connect_state":"connected","device":{"node_id":"other-node-id","call_id":"other-call-id","tag":"other-tag-id","peer":{"type":"phone","params":{"from_number":"+1555","to_number":"+1777"}}},"tag":"mocked-uuid","call_id":"call-id","node_id":"node-id"}}`)
 
     // const _playNotification = JSON.parse(`{"state":"finished","call_id":"call-id","control_id":"mocked-uuid","event_type":"${CallNotification.Play}"}`)
     // const _promptNotification = JSON.parse(`{"control_id":"mocked-uuid","call_id":"call-id","event_type":"${CallNotification.Collect}","result":{"type":"digit","params":{"digits":"12345","terminator":"#"}}}`)
-    // const _recordNotification = JSON.parse(`{"state":"finished","call_id":"call-id","control_id":"mocked-uuid","event_type":"${CallNotification.Record}","url":"record-url","record":{"audio":{"type":"digit","params":{"digits":"12345","terminator":"#"}}}}`)
-    // const _connectNotification = JSON.parse(`{"connect_state":"connected","call_id":"call-id","control_id":"mocked-uuid","event_type":"${CallNotification.Connect}"}`)
 
     beforeEach(() => {
       call.id = 'call-id'
       call.nodeId = 'node-id'
       call.state = CallState.Created
-      Connection.mockResponse.mockReturnValueOnce(JSON.parse('{"id":"c04d725a-c8bc-4b9e-bf1e-9c05150797cc","jsonrpc":"2.0","result":{"result":{"code":"200","message":"message","control_id":"control-id"}}}'))
+      // Connection.mockResponse.mockReturnValueOnce(JSON.parse('{"id":"c04d725a-c8bc-4b9e-bf1e-9c05150797cc","jsonrpc":"2.0","result":{"result":{"code":"200","message":"message","control_id":"control-id"}}}'))
     })
 
-    // it('.answered should return true if the call has been answered', () => {
-    //   call.state = 'ringing'
-    //   expect(call.answered).toBe(false)
+    it('.answered should return true if the call has been answered', () => {
+      call.state = 'ringing'
+      expect(call.answered).toBe(false)
 
-    //   call.state = 'answered'
-    //   expect(call.answered).toBe(true)
-    // })
+      call.state = 'answered'
+      expect(call.answered).toBe(true)
+    })
 
-    // it('.active should return true if the state is not in ending or ended', () => {
-    //   call.state = 'answered'
-    //   expect(call.active).toBe(true)
-    //   call.state = 'ending'
-    //   expect(call.active).toBe(false)
-    //   call.state = 'ended'
-    //   expect(call.active).toBe(false)
-    // })
+    it('.active should return true if the state is not in ending or ended', () => {
+      call.state = 'answered'
+      expect(call.active).toBe(true)
+      call.state = 'ending'
+      expect(call.active).toBe(false)
+      call.state = 'ended'
+      expect(call.active).toBe(false)
+    })
 
-    // it('.ended should return true if the state is in ending or ended', () => {
-    //   call.state = 'answered'
-    //   expect(call.ended).toBe(false)
-    //   call.state = 'ending'
-    //   expect(call.ended).toBe(true)
-    //   call.state = 'ended'
-    //   expect(call.ended).toBe(true)
-    // })
+    it('.ended should return true if the state is in ending or ended', () => {
+      call.state = 'answered'
+      expect(call.ended).toBe(false)
+      call.state = 'ending'
+      expect(call.ended).toBe(true)
+      call.state = 'ended'
+      expect(call.ended).toBe(true)
+    })
 
     it('.dial() should wait for "answered" event', done => {
+      // FIXME: Handle DialResult
       const msg = new Execute({
         protocol: 'signalwire_service_random_uuid',
         method: 'call.begin',
@@ -143,8 +144,7 @@ describe('Call', () => {
       call.dial().then(result => {
         // expect(result).toBeInstanceOf(DialResult)
         // expect(result.successful).toBe(true)
-        expect(Connection.mockSend).toHaveBeenCalledTimes(1)
-        expect(Connection.mockSend).toHaveBeenCalledWith(msg)
+        expect(Connection.mockSend).nthCalledWith(1, msg)
         done()
       })
       // @ts-ignore
@@ -163,8 +163,7 @@ describe('Call', () => {
       call.answer().then(result => {
         expect(result).toBeInstanceOf(AnswerResult)
         expect(result.successful).toBe(true)
-        expect(Connection.mockSend).toHaveBeenCalledTimes(1)
-        expect(Connection.mockSend).toHaveBeenCalledWith(msg)
+        expect(Connection.mockSend).nthCalledWith(1, msg)
         done()
       })
       // @ts-ignore
@@ -185,12 +184,127 @@ describe('Call', () => {
         expect(result).toBeInstanceOf(HangupResult)
         expect(result.successful).toBe(true)
         expect(result.reason).toEqual('busy')
-        expect(Connection.mockSend).toHaveBeenCalledTimes(1)
-        expect(Connection.mockSend).toHaveBeenCalledWith(msg)
+        expect(Connection.mockSend).nthCalledWith(1, msg)
         done()
       })
       // @ts-ignore
       session.calling.notificationHandler(_stateNotificationEnded)
+    })
+
+    describe('recording methods', () => {
+      const record = { audio: { format: 'mp3', beep: true } }
+      const getMsg = () => new Execute({
+        protocol: 'signalwire_service_random_uuid',
+        method: 'call.record',
+        params: {
+          node_id: call.nodeId,
+          call_id: call.id,
+          control_id: 'mocked-uuid',
+          record
+        }
+      })
+
+      it('.record() should wait until the recording ends', done => {
+        call.record(record).then(result => {
+          expect(result).toBeInstanceOf(RecordResult)
+          // expect(result.succeeded).toBe(true)
+          // expect(result.failed).toBe(false)
+          expect(Connection.mockSend).nthCalledWith(1, getMsg())
+          done()
+        })
+        // @ts-ignore
+        session.calling.notificationHandler(_recordNotification)
+      })
+
+      it('.recordAsync() should return a RecordAction for async control', async done => {
+        const action = await call.recordAsync(record)
+        expect(action).toBeInstanceOf(RecordAction)
+        expect(action.completed).toBe(false)
+        expect(Connection.mockSend).nthCalledWith(1, getMsg())
+        // @ts-ignore
+        session.calling.notificationHandler(_recordNotification)
+        expect(action.completed).toBe(true)
+        done()
+      })
+
+    })
+
+    describe('connect methods', () => {
+      const _tmpDevices = [
+        { type: 'phone', to: '999', from: '231', timeout: 10 },
+        { type: 'phone', to: '888', from: '234', timeout: 20 }
+      ]
+      const getMsg = (serial: boolean) => {
+        let devices = []
+        if (serial) {
+          devices = [
+            [ { type: 'phone', params: { to_number: '999', from_number: '231', timeout: 10 } } ],
+            [ { type: 'phone', params: { to_number: '888', from_number: '234', timeout: 20 } } ]
+          ]
+        } else {
+          devices = [
+            [
+              { type: 'phone', params: { to_number: '999', from_number: '231', timeout: 10 } },
+              { type: 'phone', params: { to_number: '888', from_number: '234', timeout: 20 } }
+            ]
+          ]
+        }
+        return new Execute({
+          protocol: 'signalwire_service_random_uuid',
+          method: 'call.connect',
+          params: { node_id: call.nodeId, call_id: call.id, devices }
+        })
+      }
+
+      it('.connect() in serial should wait until the call is connected', done => {
+        call.connect(..._tmpDevices).then(result => {
+          expect(result).toBeInstanceOf(ConnectResult)
+          expect(result.successful).toBe(true)
+          expect(result.result).toBe(call.peer)
+          expect(Connection.mockSend).nthCalledWith(1, getMsg(true))
+          done()
+        })
+        // @ts-ignore
+        session.calling.notificationHandler(_connectNotification)
+      })
+
+      it('.connect() in parallel should wait until the call is connected', done => {
+        call.connect(_tmpDevices).then(result => {
+          expect(result).toBeInstanceOf(ConnectResult)
+          expect(result.successful).toBe(true)
+          expect(result.result).toBe(call.peer)
+          expect(Connection.mockSend).nthCalledWith(1, getMsg(false))
+          done()
+        })
+        // @ts-ignore
+        session.calling.notificationHandler(_connectNotification)
+      })
+
+      it('.connectAsync() in serial should return a ConnectAction for async control', async done => {
+        const action = await call.connectAsync(..._tmpDevices)
+        expect(action).toBeInstanceOf(ConnectAction)
+        expect(action.completed).toBe(false)
+        expect(Connection.mockSend).nthCalledWith(1, getMsg(true))
+
+        // @ts-ignore
+        session.calling.notificationHandler(_connectNotification)
+        expect(action.completed).toBe(true)
+
+        done()
+      })
+
+      it('.connectAsync() in parallel should return a ConnectAction for async control', async done => {
+        const action = await call.connectAsync(_tmpDevices)
+        expect(action).toBeInstanceOf(ConnectAction)
+        expect(action.completed).toBe(false)
+        expect(Connection.mockSend).nthCalledWith(1, getMsg(false))
+
+        // @ts-ignore
+        session.calling.notificationHandler(_connectNotification)
+        expect(action.completed).toBe(true)
+        done()
+      })
+
     })
   })
 })

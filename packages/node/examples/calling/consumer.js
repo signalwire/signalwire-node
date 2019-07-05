d02f1a50c1c9e37ee6592d195d4349260a362da7
Edoardo Gallo <edo91.gallo@gmail.com>
2019-07-05 10:34:55 +0200

    update consumer example

--- packages/node/examples/calling/consumer.js
+++ packages/node/examples/calling/consumer.js
@@ -1,26 +1,29 @@
 const { RelayConsumer } = require('../..')
 
-
 const consumer = new RelayConsumer({
   project: '',
   token: '',
   contexts: ['home', 'office'],
-  // setup: () => {},
+  setup: (consumer) => {
+    consumer.project = ''
+    consumer.token = ''
+  },
   onIncomingCall: async (call) => {
     console.log('Inbound call', call.id, call.from, call.to)
-    try {
-      await call.answer()
-      await call.playTTSSync({ text: 'Hey Man! How you doing?' })
-      await call.playAudioSync('https://cdn.signalwire.com/default-music/welcome.mp3')
-      await call.playTTSSync({ text: 'Thanks and Good Bye!' })
-      await call.hangup()
-    } catch (error) {
-      console.error(error)
+    const answerResult = await call.answer()
+    if (!answerResult.successful) {
+      console.error('Answer Error')
+      return
     }
-  },
-  // onTask: (task) => {
-
-  // }
+    const collect = { initial_timeout: 10, digits: { max: 3, digit_timeout: 5 } }
+    const prompt = await call.promptTTS(collect, { text: 'Welcome at SignalWire! Please, enter your PIN' })
+    if (prompt.successful) {
+      await call.playTTS({ text: `You entered: ${prompt.result}. Thanks and good bye!` })
+    } else {
+      await call.playTTS({ text: 'Errors during prompt.' })
+    }
+    await call.hangup()
+  }
 })
 
 consumer.run()

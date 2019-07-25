e7e8ee7d04f95246ffbbdce72cd27c79fdf11523
Edoardo Gallo <edo91.gallo@gmail.com>
2019-07-25 19:07:39 +0200

    create base classes for Messaging, Message and SendResult

---
+++ packages/common/src/relay/messaging/Messaging.ts
@@ -0,0 +1,42 @@
+import logger from '../../util/logger'
+import { register, trigger } from '../../services/Handler'
+import { MessageNotification } from '../../util/constants/relay'
+import Relay from '../Relay'
+import { Execute } from '../../messages/Blade';
+import SendResult from './SendResult'
+import Message from './Message'
+
+export default class Messaging extends Relay {
+  protected service: string = 'messaging'
+
+  notificationHandler(notification: any) {
+    const { event_type, params } = notification
+    params.event_type = event_type
+    switch (event_type) {
+      case MessageNotification.State: {
+        // TODO: to implement
+        const message = new Message(params)
+        logger.debug('Message state!!', message)
+        break
+      }
+      case MessageNotification.Receive: {
+        const message = new Message(params)
+        trigger(this.session.relayProtocol, message, this._ctxUniqueId(message.context))
+        break
+      }
+    }
+  }
+
+  async send(params: any): Promise<SendResult> {
+    const msg = new Execute({
+      protocol: this.session.relayProtocol,
+      method: 'messaging.send',
+      params
+    })
+
+    const response: any = await this.session.execute(msg)
+    logger.debug('Send message response', response)
+    return new SendResult(response)
+  }
+
+}
---
+++ packages/common/src/relay/messaging/SendResult.ts
@@ -0,0 +1,12 @@
+export default class SendResult {
+
+  public successful: boolean
+  public messageId: string
+  // public errors
+
+  constructor(protected result: { code: string, message: string, message_id: string }) {
+    this.successful = result.code && result.code === '200'
+    this.messageId = result.message_id
+  }
+
+}

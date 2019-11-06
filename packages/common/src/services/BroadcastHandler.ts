7b77a56ba8704ee5dfde60f07d49e1abb83e224e
Edoardo Gallo <edo91.gallo@gmail.com>
2019-11-06 16:40:58 +0100

    refactoring BroadcastHandler service

--- packages/common/src/services/BroadcastHandler.ts
+++ packages/common/src/services/BroadcastHandler.ts
@@ -1,16 +1,7 @@
-import BaseSession from '../BaseSession'
 import logger from '../util/logger'
-import RelayClient from '../../../node/src/relay/'
 import VertoHandler from '../webrtc/VertoHandler'
-import { CallNotification } from '../util/constants/relay'
 
-/* tslint:disable-next-line */
-export default function BroadcastHandler(session: BaseSession, broadcastParams: any): void;
-/* tslint:disable-next-line */
-export default function BroadcastHandler(session: RelayClient, broadcastParams: any): void;
-
-/* tslint:disable-next-line */
-export default function BroadcastHandler(session: any, broadcastParams: any): void {
+export default (session: any, broadcastParams: any): void => {
   const { protocol, event, params } = broadcastParams
   const { event_type, node_id } = params
 
@@ -18,33 +9,15 @@ export default function BroadcastHandler(session: any, broadcastParams: any): vo
     return logger.error('Session protocol mismatch.')
   }
 
-  const _switchOnEventType = () => {
-    switch (event_type) {
-      case CallNotification.State:
-      case CallNotification.Receive:
-      case CallNotification.Connect:
-      case CallNotification.Record:
-      case CallNotification.Play:
-      case CallNotification.Collect:
-      case CallNotification.Fax:
-      case CallNotification.Detect:
-      case CallNotification.Tap:
-      case CallNotification.SendDigits:
-        session.calling.notificationHandler(params)
-        break
-      case 'webrtc.message':
+  switch (event) {
+    case 'queuing.relay.events':
+      if (event_type === 'webrtc.message') {
         const handler = new VertoHandler(session)
         handler.nodeId = node_id
         handler.handleMessage(params.params)
-        break
-      default:
-        return logger.error(`Unknown notification type: ${event_type}`)
-    }
-  }
-
-  switch (event) {
-    case 'queuing.relay.events':
-      _switchOnEventType()
+      } else {
+        session.calling.notificationHandler(params)
+      }
       break
     case 'queuing.relay.tasks':
       session.tasking.notificationHandler(params)

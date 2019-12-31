f5094880acf189857483637406222eabb79fc824
Edoardo Gallo <edo91.gallo@gmail.com>
2019-12-31 11:29:46 +0100

    split scopes of Jest suites

--- packages/node/tests/relay/RelayClient.test.ts
+++ packages/node/tests/relay/RelayClient.test.ts
@@ -1,9 +1,13 @@
-import behaveLikeBaseSession from '../../../common/tests/behaveLike/BaseSession'
-import { BladeDisconnect } from '../../../common/tests/behaveLike/BladeMessages'
+import behaveLikeBaseSession from '../../../common/tests/behaveLike/BaseSession.spec'
+import behaveLikeReceive from '../../../common/tests/behaveLike/Receive.spec'
+import behaveLikeSetup from '../../../common/tests/behaveLike/Setup.spec'
+import { BladeDisconnect } from '../../../common/tests/behaveLike/BladeMessages.spec'
 import RelayClient from '../../src/relay'
 
 describe('RelayClient Node', () => {
-  const instance = new RelayClient({ host: 'example.signalwire.com', project: 'project', token: 'token' })
+  const instance = new RelayClient({ project: 'project', token: 'token' })
   behaveLikeBaseSession.call(this, instance)
+  behaveLikeReceive.call(this, instance)
+  behaveLikeSetup.call(this, instance)
   BladeDisconnect.call(this, instance)
 })
--- packages/react-native/tests/Relay.test.ts
+++ packages/react-native/tests/Relay.test.ts
@@ -1,9 +1,13 @@
-import behaveLikeBaseSession from '../../common/tests/behaveLike/BaseSession'
-import { BladeDisconnect } from '../../common/tests/behaveLike/BladeMessages'
+import behaveLikeBaseSession from '../../common/tests/behaveLike/BaseSession.spec'
+import behaveLikeReceive from '../../common/tests/behaveLike/Receive.spec'
+import behaveLikeSetup from '../../common/tests/behaveLike/Setup.spec'
+import { BladeDisconnect } from '../../common/tests/behaveLike/BladeMessages.spec'
 import Relay from '../src/Relay'
 
 describe('Relay React Native', () => {
   const instance = new Relay({ host: 'example.signalwire.com', project: 'project', token: 'token' })
   behaveLikeBaseSession.call(this, instance)
+  behaveLikeReceive.call(this, instance)
+  behaveLikeSetup.call(this, instance)
   BladeDisconnect.call(this, instance)
 })

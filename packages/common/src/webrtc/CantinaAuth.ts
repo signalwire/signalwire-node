3edbdbd4815075fd3e3f5c49da41ca522d5a9131
Edoardo Gallo <edoardo@signalwire.com>
2020-02-20 18:22:26 +0100

    Cantina Auth (#217)
    * initial POC for cantina auth service
    
    * avoid duplicate code
    
    * pass hostname in body
    
    * add types/jest to the global pkg
    
    * use TS declare to avoid ts-ignore lines
    
    * simpler code and unit tests
    
    * add invite_token to guestLogin method
    
    * always use latest JS SDK in vanilla-test
    
    * passing specs
    
    * add CantinaAuth method to validate token before guest login
    
    * reject promise if response status is error
    
    * use req.status instead of req.statusText
    
    * move CantinaAuth interfaces into webrtc folder
    
    * update specs on refresh endpoint response
    
    * bump JS package version to alpha.1

---
+++ packages/common/src/webrtc/CantinaAuth.ts
@@ -0,0 +1,77 @@
+import { ICantinaAuthParams, ICantinaUser } from './interfaces'
+import logger from '../util/logger'
+
+type RefreshResponse = { project: string, jwt_token: string }
+type CheckInviteTokenResponse = { valid: boolean, name: string, config: object }
+
+const FETCH_OPTIONS: RequestInit = {
+  method: 'POST',
+  credentials: 'include',
+  headers: {
+    'Content-Type': 'application/json'
+  }
+}
+
+class CantinaAuth {
+  public baseUrl = 'https://cantina-backend.signalwire.com' // TODO: change me
+  public hostname: string
+
+  constructor(private params: ICantinaAuthParams = {}) {
+    const { hostname = location.hostname } = params
+    this.hostname = hostname
+  }
+
+  private _fetch = (url: RequestInfo, options: RequestInit) => {
+    return fetch(url, options).then(async (response: Response) => {
+      const payload = await response.json()
+      if (response.status >= 200 && response.status < 300) {
+        return payload
+      } else {
+        const errorMessage = `HTTP Request failed with status ${response.status}`
+        const error = new Error(errorMessage)
+        // @ts-ignore
+        error.payload = payload
+        return Promise.reject(error)
+      }
+    })
+  }
+
+  async userLogin(username: string, password: string): Promise<ICantinaUser> {
+    const response = await this._fetch(`${this.baseUrl}/login/user`, {
+      ...FETCH_OPTIONS,
+      body: JSON.stringify({ username, password, hostname: this.hostname })
+    })
+    logger.info('userLogin response', response)
+    return response
+  }
+
+  async guestLogin(name: string, email: string, token: string): Promise<ICantinaUser> {
+    const response = await this._fetch(`${this.baseUrl}/login/guest`, {
+      ...FETCH_OPTIONS,
+      body: JSON.stringify({ name, email, token, hostname: this.hostname })
+    })
+    logger.info('guestLogin response', response)
+    return response
+  }
+
+  async refresh(): Promise<RefreshResponse> {
+    const response = await this._fetch(`${this.baseUrl}/refresh`, {
+      ...FETCH_OPTIONS,
+      method: 'PUT',
+      body: JSON.stringify({ hostname: this.hostname })
+    })
+    logger.info('refresh response', response)
+    return response
+  }
+
+  async checkInviteToken(token: string): Promise<CheckInviteTokenResponse> {
+    const response = await this._fetch(`${this.baseUrl}/check-token`, {
+      ...FETCH_OPTIONS,
+      body: JSON.stringify({ token, hostname: this.hostname })
+    })
+    logger.info('checkInviteToken response', response)
+    return response
+  }
+}
+
+export default CantinaAuth
---
+++ packages/common/tests/webrtc/CantinaAuth.test.ts
@@ -0,0 +1,143 @@
+import CantinaAuth from '../../src/webrtc/CantinaAuth'
+
+declare var global: any;
+
+const mockFetchSuccess = (data: object) => {
+  return jest.fn().mockImplementationOnce(() => Promise.resolve({
+    ok: true,
+    status: 200,
+    json: () => data
+  }))
+}
+
+const mockFetchFailure = (error: object) => {
+  return jest.fn().mockImplementationOnce(() => Promise.resolve({
+    ok: false,
+    status: 422,
+    json: () => error
+  }))
+}
+
+const DEFAULT_FETCH_OPTIONS = {
+  method: 'POST',
+  credentials: 'include',
+  headers: {
+    'Content-Type': 'application/json'
+  }
+}
+
+describe('CantinaAuth', () => {
+  const hostname = 'jest.relay.com'
+  const errorResponse = {
+    errors: [{ detail: 'Unauthorized', code: '401' }]
+  }
+
+  let auth: CantinaAuth = null
+  beforeEach(() => {
+    auth = new CantinaAuth({ hostname })
+  })
+
+  it('should default the hostname from global location if not provided', () => {
+    const authDef = new CantinaAuth()
+    expect(authDef.hostname).toEqual('localhost')
+
+    const authCustom = new CantinaAuth({ hostname })
+    expect(authCustom.hostname).toEqual(hostname)
+
+  })
+
+  describe('userLogin', () => {
+    it('should expose userLogin to get jwt for a user', async () => {
+      global.fetch = mockFetchSuccess({ jwt_token: 'user-jwt', scopes: ['scope1', 'scope2'] })
+      const response = await auth.userLogin('username', 'password')
+
+      expect(response.jwt_token).toEqual('user-jwt')
+      expect(response.scopes).toEqual(['scope1', 'scope2'])
+      expect(global.fetch).toHaveBeenCalledTimes(1)
+      expect(global.fetch).toHaveBeenCalledWith(`${auth.baseUrl}/login/user`, {
+        ...DEFAULT_FETCH_OPTIONS,
+        body: '{"username":"username","password":"password","hostname":"jest.relay.com"}'
+      })
+    })
+
+    it('should return the error if fetch failed', async () => {
+      global.fetch = mockFetchFailure(errorResponse)
+
+      expect.assertions(2)
+      await expect(auth.userLogin('username', 'password')).rejects.toEqual(expect.any(Error))
+      expect(global.fetch).toHaveBeenCalledTimes(1)
+    })
+  })
+
+  describe('guestLogin', () => {
+    it('should expose guestLogin to get jwt for a guest', async () => {
+      global.fetch = mockFetchSuccess({ jwt_token: 'guest-jwt', scopes: ['scope3'] })
+
+      const response = await auth.guestLogin('name', 'email', 'uuid')
+      expect(response.jwt_token).toEqual('guest-jwt')
+      expect(response.scopes).toEqual(['scope3'])
+      expect(global.fetch).toHaveBeenCalledTimes(1)
+      expect(global.fetch).toHaveBeenCalledWith(`${auth.baseUrl}/login/guest`, {
+        ...DEFAULT_FETCH_OPTIONS,
+        body: '{"name":"name","email":"email","token":"uuid","hostname":"jest.relay.com"}'
+      })
+    })
+
+    it('should return the error if fetch failed', async () => {
+      global.fetch = mockFetchFailure(errorResponse)
+
+      expect.assertions(2)
+      await expect(auth.guestLogin('name', 'email', 'uuid')).rejects.toEqual(expect.any(Error))
+      expect(global.fetch).toHaveBeenCalledTimes(1)
+    })
+  })
+
+  describe('refresh', () => {
+    it('should request to refresh the JWT', async () => {
+      global.fetch = mockFetchSuccess({ project: 'project', jwt_token: 'new-jwt' })
+
+      const response = await auth.refresh()
+
+      expect(response.project).toEqual('project')
+      expect(response.jwt_token).toEqual('new-jwt')
+      expect(global.fetch).toHaveBeenCalledTimes(1)
+      expect(global.fetch).toHaveBeenCalledWith(`${auth.baseUrl}/refresh`, {
+        ...DEFAULT_FETCH_OPTIONS,
+        method: 'PUT',
+        body: '{"hostname":"jest.relay.com"}'
+      })
+    })
+
+    it('should return the error if fetch failed', async () => {
+      global.fetch = mockFetchFailure(errorResponse)
+
+      expect.assertions(2)
+      await expect(auth.refresh()).rejects.toEqual(expect.any(Error))
+      expect(global.fetch).toHaveBeenCalledTimes(1)
+    })
+  })
+
+  describe('checkInviteToken', () => {
+    it('should expose checkInviteToken to validate an invite-token from URL', async () => {
+      global.fetch = mockFetchSuccess({ valid: true, name: 'room name', config: {} })
+
+      const response = await auth.checkInviteToken('uuid')
+
+      expect(response.valid).toEqual(true)
+      expect(response.name).toEqual('room name')
+      expect(global.fetch).toHaveBeenCalledTimes(1)
+      expect(global.fetch).toHaveBeenCalledWith(`${auth.baseUrl}/check-token`, {
+        ...DEFAULT_FETCH_OPTIONS,
+        body: '{"token":"uuid","hostname":"jest.relay.com"}'
+      })
+    })
+
+    it('should return the error if fetch failed', async () => {
+      global.fetch = mockFetchFailure(errorResponse)
+
+      expect.assertions(2)
+      await expect(auth.checkInviteToken('uuid')).rejects.toEqual(expect.any(Error))
+      expect(global.fetch).toHaveBeenCalledTimes(1)
+    })
+  })
+})

b03d08315169ed4ba81134f4359d41b134b0cbb1
Jason Gallivan <59837703+natural411@users.noreply.github.com>
2020-04-02 16:24:16 -0600

    removing manual linking of packages (#244)
    * removing manual linking of packages
    
    * remove tabs
    
    * updating spaces to 2
    
    * updating prettier globals
    
    * prepare react-native@1.0.2 and changelog
    
    Co-authored-by: jason gallivan <gallivanjason@gmail.com>
    Co-authored-by: Edoardo Gallo <edo91.gallo@gmail.com>

--- packages/react-native/.scripts/postinstall.js
+++ packages/react-native/.scripts/postinstall.js
@@ -9,31 +9,29 @@ async function main() {
   const mainTasks = [
     {
       title: 'Checking react-native..',
-      task: async () => await execa('react-native', ['--version'], { cwd: cwd() }).catch(error => {
-        throw new Error('\n\n\treact-native CLI not available.\n\n')
-      })
+      task: async () =>
+        await execa('react-native', [ '--version' ], { cwd: cwd() }).catch(error => {
+          throw new Error('\n\n\treact-native CLI not available.\n\n')
+        }),
     },
     {
       title: 'Installing WebRTC engine..',
       task: async () => {
-        await execa('npm', ['install', 'react-native-webrtc'], { cwd: cwd() })
-        await execa('react-native', ['link', 'react-native-webrtc'], { cwd: cwd() })
-      }
+        await execa('npm', [ 'install', 'react-native-webrtc' ], { cwd: cwd() })
+      },
     },
     {
       title: 'Installing InCall Manager..',
       task: async () => {
-        await execa('npm', ['install', 'react-native-incall-manager'], { cwd: cwd() })
-        await execa('react-native', ['link', 'react-native-incall-manager'], { cwd: cwd() })
-      }
+        await execa('npm', [ 'install', 'react-native-incall-manager' ], { cwd: cwd() })
+      },
     },
     {
       title: 'Installing AsyncStorage..',
       task: async () => {
-        await execa('npm', ['install', '@react-native-community/async-storage'], { cwd: cwd() })
-        await execa('react-native', ['link', '@react-native-community/async-storage'], { cwd: cwd() })
-      }
-    }
+        await execa('npm', [ 'install', '@react-native-community/async-storage' ], { cwd: cwd() })
+      },
+    },
   ]
 
   await new Listr(mainTasks, { showSubtasks: true }).run().catch(e => console.error(e.message))

0bc6f3bf1d581e944057b264db561b8527189c30
Edoardo Gallo <edo91.gallo@gmail.com>
2020-05-16 01:12:20 +0200

    publish js alpha.8.3

--- packages/js/index.ts
+++ packages/js/index.ts
@@ -3,7 +3,7 @@ import Verto from './src/Verto'
 import { setAgentName } from '../common/src/messages/blade/Connect'
 import CantinaAuth from '../common/src/webrtc/CantinaAuth'
 
-export const VERSION = '1.3.0-alpha.8.2'
+export const VERSION = '1.3.0-alpha.8.3'
 setAgentName(`JavaScript SDK/${VERSION}`)
 
 export {

781f2389d1fd812f1e71ecac4d2aa8b285cdc324
Piotr Gregor <piotr@dataandsignal.com>
2020-05-21 14:10:39 +0000

    Add calling file/line to log output

--- packages/common/src/util/logger.ts
+++ packages/common/src/util/logger.ts
@@ -1,4 +1,5 @@
 import log from 'loglevel'
+
 const datetime = () => new Date().toISOString().replace('T', ' ').replace('Z', '')
 const logger = log.getLogger('signalwire')
 
@@ -7,7 +8,26 @@ logger.methodFactory = (methodName, logLevel, loggerName) => {
   const rawMethod = originalFactory(methodName, logLevel, loggerName)
   // tslint:disable-next-line
   return function () {
-    const messages = [datetime(), '-']
+
+    const messages = [datetime(), ' : ']
+
+    {
+        let err = new Error()
+    
+        if (err) {
+            let stack = err.stack
+            if (stack) {
+                let stacksp = stack.split("at ")
+                if (stacksp.length > 3) {
+                    let logLineDetails = stacksp[2].trim()
+                    messages.push(logLineDetails)
+                }
+            }
+        }
+    }
+
+    messages.push('\n')
+
     for (let i = 0; i < arguments.length; i++) {
       messages.push(arguments[i])
     }

d105349d2818415b32b68d61f332e8b9a3c3b127
Edoardo Gallo <edo91.gallo@gmail.com>
2020-06-03 11:03:38 +0200

    publish RN package alpha.3

--- packages/react-native/index.ts
+++ packages/react-native/index.ts
@@ -3,7 +3,7 @@ import Verto from '../js/src/Verto'
 import { setAgentName } from '../common/src/messages/blade/Connect'
 import CantinaAuth from '../common/src/webrtc/CantinaAuth'
 
-export const VERSION = '1.1.0-alpha.2'
+export const VERSION = '1.1.0-alpha.3'
 setAgentName(`ReactNative SDK/${VERSION}`)
 
 export {

d792bb97d24dfb956622da40866ad416f6911c1c
Edoardo Gallo <edo91.gallo@gmail.com>
2020-10-20 13:34:18 +0200

    release JS SDK cantina.17

--- packages/js/index.ts
+++ packages/js/index.ts
@@ -3,7 +3,7 @@ import Verto from './src/Verto'
 import { setAgentName } from '../common/src/messages/blade/Connect'
 import CantinaAuth from '../common/src/webrtc/CantinaAuth'
 
-export const VERSION = '1.3.0-cantina.16'
+export const VERSION = '1.3.0-cantina.17'
 setAgentName(`JavaScript SDK/${VERSION}`)
 
 export {

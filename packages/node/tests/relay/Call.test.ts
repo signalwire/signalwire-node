3e2f24f62173827173ca24e715191d89f6dcd29c
Aye Min Aung <ayemin@signalwire.com>
2021-09-15 23:52:33 +0700

    wrong auto imported path to dist in Call.test.ts fixed

--- packages/node/tests/relay/Call.test.ts
+++ packages/node/tests/relay/Call.test.ts
@@ -6,7 +6,7 @@ import { Execute } from '../../../common/src/messages/Blade'
 const Connection = require('../../../common/src/services/Connection')
 import { RecordAction, PlayAction, PromptAction, ConnectAction, FaxAction, DetectAction, TapAction, SendDigitsAction } from '../../../common/src/relay/calling/actions'
 import { HangupResult, RecordResult, AnswerResult, PlayResult, PromptResult, ConnectResult, DialResult, FaxResult, DetectResult, TapResult, SendDigitsResult, DisconnectResult } from '../../../common/src/relay/calling/results'
-import { DeepArray } from '../../dist/common/src/util/interfaces'
+import { DeepArray } from '../../../common/src/util/interfaces'
 jest.mock('../../../common/src/services/Connection')
 
 describe('Call', () => {

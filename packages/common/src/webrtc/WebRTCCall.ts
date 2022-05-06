2f154f4c9412c6da9d2e09791a035333a18fff7e
Edoardo Gallo <edo91.gallo@gmail.com>
2022-05-06 19:23:07 +0200

    comment old code

--- packages/common/src/webrtc/WebRTCCall.ts
+++ packages/common/src/webrtc/WebRTCCall.ts
@@ -505,23 +505,23 @@ export default abstract class WebRTCCall {
     toggleAudioTracks(this.options.remoteStream)
   }
 
-  sfuLowResolution(streamId: string, trackId: string) {
-    console.debug('Set video res to low')
-    const msg = new Modify({ ...this.messagePayload, action: 'set-sfu-low-res', streamId, trackId })
-    return this._execute(msg)
-  }
+  // sfuLowResolution(streamId: string, trackId: string) {
+  //   console.debug('Set video res to low')
+  //   const msg = new Modify({ ...this.messagePayload, action: 'set-sfu-low-res', streamId, trackId })
+  //   return this._execute(msg)
+  // }
 
-  sfuHighResolution(streamId: string, trackId: string) {
-    console.debug('Set video res to high')
-    const msg = new Modify({ ...this.messagePayload, action: 'set-sfu-high-res', streamId, trackId })
-    return this._execute(msg)
-  }
+  // sfuHighResolution(streamId: string, trackId: string) {
+  //   console.debug('Set video res to high')
+  //   const msg = new Modify({ ...this.messagePayload, action: 'set-sfu-high-res', streamId, trackId })
+  //   return this._execute(msg)
+  // }
 
-  sfuDefaultResolution(streamId: string, trackId: string) {
-    console.debug('Set video res to default')
-    const msg = new Modify({ ...this.messagePayload, action: 'set-sfu-default-res', streamId, trackId })
-    return this._execute(msg)
-  }
+  // sfuDefaultResolution(streamId: string, trackId: string) {
+  //   console.debug('Set video res to default')
+  //   const msg = new Modify({ ...this.messagePayload, action: 'set-sfu-default-res', streamId, trackId })
+  //   return this._execute(msg)
+  // }
 
   doReinviteWithRelayOnly() {
     if (this.peer && this.active) {

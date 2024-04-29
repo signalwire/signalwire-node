4f164ceea676f9c0ed26b100aee782b787d93c96
Ammar Ansari <iammaransari@gmail.com>
2024-04-29 17:17:47 +0200

    Fix babel-preset-env vulnerability

--- packages/js/webpack.config.js
+++ packages/js/webpack.config.js
@@ -1,52 +1,55 @@
-const webpack = require('webpack')
-const VERSION = require('./package.json').version
+const webpack = require('webpack');
+const VERSION = require('./package.json').version;
 
 module.exports = (env, argv) => {
-  const outputDir = __dirname + '/dist'
-  const mode = JSON.stringify(argv.mode)
+  const outputDir = __dirname + '/dist';
+  const mode = JSON.stringify(argv.mode);
   const config = {
     mode,
     entry: './index.ts',
     output: {
       path: outputDir,
       filename: 'index.min.js',
-      libraryTarget: 'this'
+      libraryTarget: 'this',
     },
     resolve: {
-      extensions: ['.ts', '.js']
+      extensions: ['.ts', '.js'],
     },
     module: {
       rules: [
         {
           test: /\.tsx?$/,
-          loader: 'babel-loader'
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+          },
         },
         {
           test: /\.tsx?$/,
           loader: 'ts-loader',
           options: {
-            configFile: 'tsconfig.es5.json'
-          }
-        }
-      ]
+            configFile: 'tsconfig.es5.json',
+          },
+        },
+      ],
     },
     devServer: {
       contentBase: [outputDir, __dirname + '/examples/vanilla-calling'],
       compress: true,
       port: 9000,
-      https: true
+      https: true,
     },
     plugins: [
       new webpack.DefinePlugin({
         'process.env': {
           NODE_ENV: mode,
-        }
+        },
       }),
       new webpack.BannerPlugin({
-        banner: `Relay SDK for JavaScript v${VERSION} (https://signalwire.com)\nCopyright 2018-2019 SignalWire\nLicensed under MIT(https://github.com/signalwire/signalwire-node/blob/master/LICENSE)`
-      })
-    ]
-  }
+        banner: `Relay SDK for JavaScript v${VERSION} (https://signalwire.com)\nCopyright 2018-2019 SignalWire\nLicensed under MIT(https://github.com/signalwire/signalwire-node/blob/master/LICENSE)`,
+      }),
+    ],
+  };
 
-  return config
-}
+  return config;
+};
